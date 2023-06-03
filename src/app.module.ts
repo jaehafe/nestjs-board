d72acd38dd3ff0737f6cc00416c42914fda749dc
Jaeha Lee <wogkdkrm112@gmail.com>
2023-06-04 05:03:50 +0900

    feat: boards - getAllBoards 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { BoardsController } from './boards/boards.controller';
 
 @Module({
   imports: [BoardsModule],
-  controllers: [BoardsController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
