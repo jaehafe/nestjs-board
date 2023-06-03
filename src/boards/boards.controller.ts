17e002fae9c5971b9b814978616fa9d3452bc1d5
Jaeha Lee <wogkdkrm112@gmail.com>
2023-06-04 05:48:22 +0900

    feat: 게시물 생성을 위한 Dto 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -12,10 +13,7 @@ export class BoardsController {
   }
 
   @Post('/')
-  createBoards(
-    @Body('title') title: string,
-    @Body('description') description: string,
-  ): Board {
-    return this.boardsService.createBoard(title, description);
+  createBoards(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardsService.createBoard(createBoardDto);
   }
 }
