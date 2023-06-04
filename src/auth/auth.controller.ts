f9364f7ba7ee32d1968e9664f875c077af7acfec
Jaeha Lee <wogkdkrm112@gmail.com>
2023-06-05 03:50:57 +0900

    feat: swagger 설치 및 auth.controller.ts 적용

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,23 @@
 import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
 
+@ApiTags('AUTH')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiResponse({
+    type: AuthCredentialsDto,
+  })
+  @ApiResponse({
+    status: 500,
+    description: '서버 에러',
+    type: AuthCredentialsDto,
+  })
+  @ApiOperation({ summary: '회원가입' })
   @Post('/signup')
   signup(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Board App API')
+    .setDescription('Board App Api 문서')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
+  console.log(`listening on port ${port}`);
 }
 bootstrap();
