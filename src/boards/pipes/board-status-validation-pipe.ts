cc5e5e48c62fad27e926240ab69f1c5e38fcdc99
Jaeha Lee <wogkdkrm112@gmail.com>
2023-06-04 15:30:11 +0900

    feat: 게시판 상태 업데이트를 위한 커스텀 파이프(BoardStatusValidationPipe) 설정

---
+++ src/boards/pipes/board-status-validation-pipe.ts
@@ -0,0 +1,25 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { BoardStatus } from '../board.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PUBLIC, BoardStatus.PRIVATE];
+
+  private isStatusValid(status: any) {
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
+  }
+
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} isn't in the status options`);
+    }
+
+    return value;
+  }
+}
