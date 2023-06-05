93168603f401d923c6d48b93604eedab4f6febce
Jaeha Lee <wogkdkrm112@gmail.com>
2023-06-05 21:03:35 +0900

    feat: User, Board Entity 관계설정

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,7 +1,9 @@
+import { Board } from 'src/boards/board.entity';
 import {
   BaseEntity,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   Unique,
 } from 'typeorm';
@@ -17,4 +19,8 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  // eager: true User정보를 가져올 떄 Board정보도 같이 가져옴
+  @OneToMany(() => Board, (board) => board.user, { eager: true })
+  boards: Board[];
 }
--- src/boards/board.entity.ts
+++ src/boards/board.entity.ts
@@ -1,4 +1,11 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/auth/user.entity';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { BoardStatus } from './board-status.enum';
 
 @Entity()
@@ -14,4 +21,8 @@ export class Board extends BaseEntity {
 
   @Column()
   status: BoardStatus;
+
+  // eager: false Board정보를 가져올 떄 User 정보도 같이 가져옴
+  @ManyToOne(() => User, (user) => user.boards, { eager: false })
+  user: User;
 }
